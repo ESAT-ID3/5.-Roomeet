6038e7ac84dc046ecaa7b44c397c5874ea9da26b
Javier Del Rey <javifotorey@gmail.com>
2025-05-19 18:20:47 +0200

    Arreglo Advertisement

--- src/components/ads/Ads.tsx
+++ src/components/ads/Ads.tsx
@@ -3,7 +3,7 @@ import "./Ads.css"
 export const Ads = () => {
 
     return (
-        <div className="ads__container">
+        <div className="ads__container2">
             <p>Advertisement</p>
             <div className="ads_image"></div>
         </div>
