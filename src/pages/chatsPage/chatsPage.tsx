4b257805d5c692c325da1ffb9be101c654ef9f60
Javier Del Rey <javifotorey@gmail.com>
2025-06-06 18:11:21 +0200

    Update chatsPage.tsx

--- src/pages/chatsPage/chatsPage.tsx
+++ src/pages/chatsPage/chatsPage.tsx
@@ -3,7 +3,7 @@ import { MessageHeader } from "../../components/messageHeader/MessageHeader";
 import { SearchBarChats } from "../../components/searchBarChats/SearchBarChats";
 import { ChatMsg } from "../../components/chat-msg/ChatMsg";
 import { SwipeNavHeader } from "../../components/app-superior-nav/swipe/SwipeNavHeader";
-import "./ChatsPage.css";
+import "./chatsPage.css"
 
 export const ChatsPage = () => {
   return (
