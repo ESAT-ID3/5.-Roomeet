d567e896e6309ad71ffe07bca5d931b9bdfd0d91
Javier <javifotorey@gmail.com>
2025-06-10 19:17:56 +0200

    Continuación Landing

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -8,12 +8,14 @@ export const Landing = () => {
     return (
         <div className="landing_container">
             <div className="landing_hero">
-                
+                <h1>Encuentra tu compañero de piso ideal en un swipe</h1>
+                <h2>Haz match con personas afines a tu estilo de vida y encuentra a tu próximo compañero de piso</h2>
+                <div>
+                    <Button/>
+                </div>
             </div>
 
-            <section className="carroussel">
-                <CarrousselLanding/>
-            </section>
+            
 
         </div>
     );
